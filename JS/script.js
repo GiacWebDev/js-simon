f43447f1325665ea05e78bca3a2121184e79517a
GiacWebDev <giacobechini@gmail.com>
2023-09-20 17:33:40 +0200

    [edit] to fix display none h1

--- JS/script.js
+++ JS/script.js
@@ -10,17 +10,14 @@
 // ######################
 
 // Elementi
-const numeriUtente = parseInt(prompt('Inserisci il numero'));
-const arrNumeriUtente = []
 
+const arrNumeriUtente = []
 const numeriRandom = randomNumbers();
 const numeriOutput = document.querySelector('h1');
 const seconds = 5
 let counter = seconds;
 
 numeriOutput.innerHTML = numeriRandom;
-
-
 
 
 // generazione 5 numeri casuali su 100
@@ -46,7 +43,23 @@ function countDown() {
     if (counter === 0) {
       clearInterval(clock);
       numeriOutput.style.display = "none";
+      richiediNumeriUtente();
     }
   },1000)
 }
 countDown()
+
+
+
+// richiedo numeri utente
+function richiediNumeriUtente() {
+  while (arrNumeriUtente.length < 5) {
+    const numeroUtente = parseInt(prompt('Inserisci un numero'));
+    
+    if (!isNaN(numeroUtente)) {
+      arrNumeriUtente.push(numeroUtente);
+    } else {
+      alert('Devi inserire un numero valido.');
+    }
+  }
+}
