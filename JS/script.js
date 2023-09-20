dfc98f13e9405228f12a3c27aa1b8429030834a0
GiacWebDev <giacobechini@gmail.com>
2023-09-20 18:25:57 +0200

    [fix] bug prompt input number [add] confrontaNumeri function

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 
 
-// 2. creare math random per 5 (su 100) numeri casuali e inserirli in un array (lista)
+// 1. generatore random per 5 (su 100) numeri casuali e inserirli in un array (lista)
 // 2. stampare in pagina l'array (appena caricata la pagina)
 // 3. creare funzione timer con cui fare sparire i numeri casuali stampati (array) dopo 5 secondi.
 // 4. attivare un prompt dopo 5 secondi (quando spariscono i numeri in pagina)
@@ -14,13 +14,19 @@
 const arrNumeriUtente = []
 const numeriRandom = randomNumbers();
 const numeriOutput = document.querySelector('h1');
+const messaggioEl = document.querySelector('h3')
 const seconds = 5
 let counter = seconds;
 
 numeriOutput.innerHTML = numeriRandom;
 
 
-// generazione 5 numeri casuali su 100
+
+// ############# FUNZIONI ###############
+
+
+
+// Generazione 5 numeri casuali su 100
 function randomNumbers() {
   const numeriCasuali = [];
   while (numeriCasuali.length < 5) {
@@ -33,7 +39,7 @@ console.log(numeriRandom);
 
 
 
-// funzione timer 5 sec
+// Timer 5 secondi
 function countDown() {
   const clock = setInterval(function(){
     console.log(counter)
@@ -43,15 +49,34 @@ function countDown() {
     if (counter === 0) {
       clearInterval(clock);
       numeriOutput.style.display = "none";
-      richiediNumeriUtente();
+
+        setTimeout(function () {
+          richiediNumeriUtente();
+        }, 500);
     }
   },1000)
 }
 countDown()
 
+// Confronta i numeri casuali con quelli inseriti dall'utente
+function confrontaNumeri() {
+  const numeriIndovinati = [];
 
+  for (const numeroUtente of arrNumeriUtente) {
+    if (numeriRandom.includes(numeroUtente)) {
+      numeriIndovinati.push(numeroUtente);
+    }
+  }
 
-// richiedo numeri utente
+  // Stampa il numero di numeri indovinati 
+  if (numeriIndovinati.length === 0) {
+    messaggioEl.innerHTML = 'Non hai indovinato nessun numero.';
+  } else {
+    messaggioEl.innerHTML = `Hai indovinato ${numeriIndovinati.length} numero/i: ${numeriIndovinati.join(', ')}`;
+  }
+}
+
+// Richiede numeri utente
 function richiediNumeriUtente() {
   while (arrNumeriUtente.length < 5) {
     const numeroUtente = parseInt(prompt('Inserisci un numero'));
@@ -62,4 +87,6 @@ function richiediNumeriUtente() {
       alert('Devi inserire un numero valido.');
     }
   }
+  // confronta i numeri
+  confrontaNumeri(); 
 }
